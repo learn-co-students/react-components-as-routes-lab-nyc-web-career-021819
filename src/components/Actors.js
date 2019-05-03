3116535c27c7d953d2a48dcd18b7897b31deb27d
David Korff <dkorff12@gmail.com>
2019-05-02 22:51:01 -0400

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import { actors } from '../data';
+import { v4 } from 'uuid';
+
+const renderMovies = (movies) => {
+  return movies.map(movie => <li key={ v4() }>{movie}</li>)
+}
+
+const renderActors = () => {
+  return actors.map(actor => {
+    return (
+      <div key={ v4() }>
+        <h2>Name: {actor.name}</h2>
+        <p>Movies:</p>
+        <ul>{renderMovies(actor.movies)}</ul>
+      </div>
+    )
+  })
+}
+
 
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {renderActors()}
     </div>
   );
-};
+}
 
 export default Actors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import { movies } from '../data';
+import { v4 } from 'uuid';
+
+
+const renderGenres = (movie) => {
+  return movie.genres.map(genre => <li key={ v4() }>{genre}</li>)
+}
+
+const renderMovies = () => {
+  return movies.map(movie => {
+    return (
+      <div key={ v4() }>
+        <h3>Name: {movie.title}</h3>
+        <p>Time: {movie.time}</p>
+        <p>Genres:</p>
+          <ul>
+            {renderGenres(movie)}
+          </ul>
+      </div>
+    )
+  })
+}
+
 
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Pages</h1>
+        {renderMovies()}
     </div>
   );
 };
